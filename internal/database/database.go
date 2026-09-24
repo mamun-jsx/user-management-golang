b1afe67ecf49d35a827d2beb116bee93eaebf987
almamun528 <ayapths@gmail.com>
2026-09-24 23:15:10 +0600

    write model of user

---
+++ internal/database/database.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func DatabaseConnection(){
+// 	db, err := gorm.Open(postgres.New(postgres.Config{
+//   DSN: "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai", // data source name, refer https://github.com/jackc/pgx
+//   PreferSimpleProtocol: true, // disables implicit prepared statement usage. By default pgx automatically uses the extended protocol
+// }), &gorm.Config{})
+}
---
+++ internal/models/user_model.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	Email     string    `json:"email"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
